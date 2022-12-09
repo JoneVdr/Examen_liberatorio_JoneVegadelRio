a32e940436999644f00a9c2de0d24b11d174481e
jone <jvegadel@myuax.com>
2022-12-09 10:31:01 +0100

    q

--- ejer4.py
+++ ejer4.py
@@ -1,8 +1,11 @@
+n = int(input("Ingrese un numero: "))
 def fibonacci(n):
-    n = int(input("Ingrese un numero: "))
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    while True:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+            break
+print(fibonacci(n))
